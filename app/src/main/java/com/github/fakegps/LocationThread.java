73fcb8ccc50d45968835f94f1808f7c70af03007
Sven Killig <sven@killig.de>
2019-09-19 02:31:07 +0200

    WifiLock

--- app/src/main/java/com/github/fakegps/LocationThread.java
+++ app/src/main/java/com/github/fakegps/LocationThread.java
@@ -5,6 +5,7 @@ import android.content.SharedPreferences;
 import android.location.ILocationManager;
 import android.location.Location;
 import android.location.LocationManager;
+import android.net.wifi.WifiManager;
 import android.os.Build;
 import android.os.Handler;
 import android.os.HandlerThread;
@@ -20,6 +21,7 @@ import java.io.IOException;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.net.InetAddress;
 
 import de.taimos.gpsd4java.api.ObjectListener;
 import de.taimos.gpsd4java.backend.GPSdEndpoint;
@@ -43,7 +45,6 @@ public class LocationThread extends HandlerThread {
 
     private static final String TAG = "LocationThread";
 
-
     private Context mContext;
     private JoyStickManager mJoyStickManager;
     private LocationManager mLocationManager;
@@ -54,8 +55,11 @@ public class LocationThread extends HandlerThread {
     private static Method mMethodMakeComplete;
     private static ILocationManager mILocationManager;
 
+    private WifiManager.WifiLock wifiLock;
     GPSdEndpoint ep;
     SharedPreferences sharedPref;
+    InetAddress ia;
+    boolean skip;
 
     public LocationThread(Context context, JoyStickManager joyStickManager) {
         super("LocationThread");
@@ -83,11 +87,15 @@ public class LocationThread extends HandlerThread {
         }
 
 
+        WifiManager wm = (WifiManager) mContext.getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        wifiLock = wm.createWifiLock(/*WifiManager.WIFI_MODE_FULL,*/ "GPSdWifiLock");
+        wifiLock.acquire();
         try {
             sharedPref= PreferenceManager.getDefaultSharedPreferences(mContext);
             String value = sharedPref.getString(MainActivity.PREFS_KEY, MainActivity.PREFS_DEFAULT);
             final String[] parts = value.split(":");
             ep = new GPSdEndpoint(parts[0], Integer.parseInt(parts[1]), new ResultParser());
+            ia=InetAddress.getByName(parts[0]);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -99,6 +107,85 @@ public class LocationThread extends HandlerThread {
 
         mHandler = new Handler(getLooper());
         mHandler.post(mUpdateLocation);
+
+
+        ep.addListener(new ObjectListener() {
+
+            @Override
+            public void handleTPV(final TPVObject tpv) {
+                skip=true;
+                Logger.i(TAG, "TPV: "+tpv);
+
+                if(tpv.getMode() != ENMEAMode.NotSeen && tpv.getMode() != ENMEAMode.NoFix) {
+                    Location location = new Location("gps");
+                    location.setLatitude(tpv.getLatitude());
+                    location.setLongitude(tpv.getLongitude());
+                    location.setAltitude(tpv.getAltitude());
+                    if (Build.VERSION.SDK_INT > 16) {
+                        location.setElapsedRealtimeNanos(SystemClock.elapsedRealtimeNanos());
+                    }
+                    location.setAccuracy((float) (tpv.getLatitudeError() + tpv.getLongitudeError()) / 2);
+                    location.setSpeed((float) tpv.getSpeed());
+                    location.setBearing((float) tpv.getCourse());
+                    location.setTime((long) tpv.getTimestamp() * 1000);
+
+                    mJoyStickManager.showJoyStick(Math.round(location.getAccuracy())+" m", location.getLatitude()+"°, "+location.getLongitude()+"° ");
+                    if (mMethodMakeComplete != null) {
+                        try {
+                            mMethodMakeComplete.invoke(location, new Object[0]);
+                        } catch (IllegalArgumentException | IllegalAccessException | InvocationTargetException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    try {
+                        mILocationManager.reportLocation(location, false);
+                    } catch (RemoteException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+            @Override
+            public void handleSKY(final SKYObject sky) {
+                Logger.i(TAG, "SKY: "+sky);
+                for (final SATObject sat : sky.getSatellites()) {
+//                        Logger.i(TAG, "  SAT: "+sat);
+                }
+            }
+
+            @Override
+            public void handleSUBFRAME(final SUBFRAMEObject subframe) {
+                Logger.i(TAG, "SUBFRAME: "+subframe);
+            }
+
+            @Override
+            public void handleATT(final ATTObject att) {
+                Logger.i(TAG, "ATT: "+att);
+            }
+
+            @Override
+            public void handleDevice(final DeviceObject device) {
+                Logger.i(TAG, "Device: "+device);
+            }
+
+            @Override
+            public void handleDevices(final DevicesObject devices) {
+                for (final DeviceObject d : devices.getDevices()) {
+                    Logger.i(TAG, "Device: "+d);
+                }
+            }
+        });
+
+        ep.start();
+
+        try {
+            Logger.i(TAG, "Version: "+ep.version());
+            Logger.i(TAG, "Watch: "+ep.watch(true, true));
+            Logger.i(TAG, "Poll: "+ep.poll());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public void startThread() {
@@ -111,6 +198,7 @@ public class LocationThread extends HandlerThread {
             quit();
             interrupt();
             ep.stop();
+            if (wifiLock.isHeld()) wifiLock.release();
         } catch (Exception e) {
             Logger.e(TAG, "stopThread fail!", e);
         }
@@ -129,83 +217,19 @@ public class LocationThread extends HandlerThread {
     }
 
     Runnable mUpdateLocation = new Runnable() {
+        static final int DELAY=1000;
         @Override
         public void run() {
-            ep.addListener(new ObjectListener() {
-
-                @Override
-                public void handleTPV(final TPVObject tpv) {
-                    Logger.i(TAG, "TPV: "+tpv);
-
-                    if(tpv.getMode() != ENMEAMode.NotSeen && tpv.getMode() != ENMEAMode.NoFix) {
-                        Location location = new Location("gps");
-                        location.setLatitude(tpv.getLatitude());
-                        location.setLongitude(tpv.getLongitude());
-                        location.setAltitude(tpv.getAltitude());
-                        if (Build.VERSION.SDK_INT > 16) {
-                            location.setElapsedRealtimeNanos(SystemClock.elapsedRealtimeNanos());
-                        }
-                        location.setAccuracy((float) (tpv.getLatitudeError() + tpv.getLongitudeError()) / 2);
-                        location.setSpeed((float) tpv.getSpeed());
-                        location.setBearing((float) tpv.getCourse());
-                        location.setTime((long) tpv.getTimestamp() * 1000);
-                        if (mMethodMakeComplete != null) {
-                            try {
-                                mMethodMakeComplete.invoke(location, new Object[0]);
-                            } catch (IllegalArgumentException | IllegalAccessException | InvocationTargetException e) {
-                                e.printStackTrace();
-                            }
-                        }
-
-                        try {
-                            mILocationManager.reportLocation(location, false);
-                        } catch (RemoteException e) {
-                            e.printStackTrace();
-                        }
-                    }
+            if(skip) {
+                skip=false;
+            } else {
+                try {
+                    Logger.d(TAG, "isReachable:" + ia.isReachable(DELAY / 2));
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
-
-                @Override
-                public void handleSKY(final SKYObject sky) {
-                    Logger.i(TAG, "SKY: "+sky);
-                    for (final SATObject sat : sky.getSatellites()) {
-//                        Logger.i(TAG, "  SAT: "+sat);
-                    }
-                }
-
-                @Override
-                public void handleSUBFRAME(final SUBFRAMEObject subframe) {
-                    Logger.i(TAG, "SUBFRAME: "+subframe);
-                }
-
-                @Override
-                public void handleATT(final ATTObject att) {
-                    Logger.i(TAG, "ATT: "+att);
-                }
-
-                @Override
-                public void handleDevice(final DeviceObject device) {
-                    Logger.i(TAG, "Device: "+device);
-                }
-
-                @Override
-                public void handleDevices(final DevicesObject devices) {
-                    for (final DeviceObject d : devices.getDevices()) {
-                        Logger.i(TAG, "Device: "+d);
-                    }
-                }
-            });
-
-            ep.start();
-
-            try {
-                Logger.i(TAG, "Version: "+ep.version());
-                Logger.i(TAG, "Watch: "+ep.watch(true, true));
-                Logger.i(TAG, "Poll: "+ep.poll());
-            } catch (Exception e) {
-                e.printStackTrace();
             }
-
+            mHandler.postDelayed(mUpdateLocation, DELAY);
         }
     };
 
